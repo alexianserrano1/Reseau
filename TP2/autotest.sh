eb8dccb7c8123b8621f4d9f3f5f94726109645ae
YannisNaidja <43177308+YannisNaidja@users.noreply.github.com>
2019-02-27 20:35:28 +0100

    Add files via upload

--- TP2/autotest.sh
+++ TP2/autotest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
-
+##### 
+ 
 EXEC=ClientUDP
+
+
 
 
 if (($#<2))
@@ -11,16 +13,22 @@ then
 	echo usage : ./autotest typeserv java ou netcat typeclient c ou java
 fi
 
+
+
+
+
 if [ "$1" = "netcat" ]
 	then
-	echo ouverture terminal server
+	echo ouverture netcat server
 	gnome-terminal -x nc -u -l 1234
 	if [ "$2" = "c" ]
 	then
+	gcc ClientUDP.c -o ClientUDP
 	echo client c donnez le message a transmettre
 	./$EXEC localhost 1234
 	elif [ "$2" = "java" ]
 	then
+	javac Client.java
 	echo client java donnez le message a transmettre
 	java Client localhost 1234
 	fi
@@ -28,13 +36,17 @@ fi
 
 if [ "$1" = "java" ]
 	then
+	javac Server.java
+	echo ouverture java server 
 	gnome-terminal -x java Server 1234
 	if [ "$2" = "c" ]
 	then
+	gcc ClientUDP.c -o ClientUDP
 	echo client c donnez le message a transmettre
 	./$EXEC localhost 1234
 	elif [ "$2" = "java" ]
 	then
+	javac Client.java
 	echo client java donnez le message a transmettre
 	java Client localhost 1234
 	fi
